89ab4516623de3e058e2029374dd4f6df34ae309
Gabriel Bran <bra23590@uvg.edu.gt>
2023-08-19 18:43:50 -0600

    documentación de TicketComprador

--- TicketComprador.java
+++ TicketComprador.java
@@ -1,30 +1,66 @@
+/**
+ * La clase TicketComprador representa a un comprador de boletos con información relevante.
+ */
 public class TicketComprador {
     private String nombre;
     private String email;
     private int numBoletos;
     private int presupuesto;
 
+    /**
+     * Constructor para inicializar un objeto TicketComprador con información básica.
+     *
+     * @param nombre     El nombre del comprador.
+     * @param email      El correo electrónico del comprador.
+     * @param presupuesto El presupuesto disponible del comprador.
+     */
     public TicketComprador(String nombre, String email, int presupuesto) {
         this.nombre = nombre;
         this.email = email;
         this.presupuesto = presupuesto;
     }
+
+    /**
+     * Establece la cantidad de boletos que el comprador desea adquirir.
+     *
+     * @param numBoletos La cantidad de boletos a establecer.
+     */
     public void setNumBoletos(int numBoletos){
         this.numBoletos = numBoletos;
     }
 
+    /**
+     * Obtiene el nombre del comprador.
+     *
+     * @return El nombre del comprador.
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * Obtiene el correo electrónico del comprador.
+     *
+     * @return El correo electrónico del comprador.
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Obtiene la cantidad de boletos que el comprador desea adquirir.
+     *
+     * @return La cantidad de boletos que el comprador desea adquirir.
+     */
     public int getNumBoletos() {
         return numBoletos;
     }
 
+    /**
+     * Obtiene el presupuesto disponible del comprador.
+     *
+     * @return El presupuesto disponible del comprador.
+     */
     public int getPresupuesto() {
         return presupuesto;
     }
