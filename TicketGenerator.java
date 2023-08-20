a80e5023240e2b07f05998d33a2b1550969c7c32
Gabriel Bran <bra23590@uvg.edu.gt>
2023-08-19 18:45:18 -0600

    documentación de TicketGenerator

--- TicketGenerator.java
+++ TicketGenerator.java
@@ -1,12 +1,26 @@
 import java.util.Random;
 
+/**
+ * Esta clase proporciona funcionalidad para generar y validar tickets.
+ */
 public class TicketGenerator {
     private static Random random = new Random();
 
+    /**
+     * Genera un número de ticket aleatorio.
+     *
+     * @return Número de ticket generado.
+     */
     public static int generarTicket() {
         return random.nextInt(15000) + 1;
     }
 
+    /**
+     * Valida si un número de ticket está dentro del rango permitido.
+     *
+     * @param ticket Número de ticket a validar.
+     * @return true si el ticket es válido, false en caso contrario.
+     */
     public static boolean validarTicket(int ticket) {
         int a = random.nextInt(15000) + 1;
         int b = random.nextInt(15000) + 1;
